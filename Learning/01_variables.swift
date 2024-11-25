a00d7cffffe753f5fbfb5801880d67eecb4a6a96
Mendel Cyprys <mmcyprys@gmail.com>
2024-11-25 10:29:51 +0000

    Testing commit from Xcode

---
+++ Learning/01_variables.swift
@@ -0,0 +1,18 @@
+//
+//  01 variables.swift
+//  Learning
+//
+//  Created by Eric Cyprys on 25/11/2024.
+//
+
+// a function wrapping the lessons contents
+func variables() {
+    // 'let' creates constants
+    let name = "Eric"
+    // 'var' creates mutable variables
+    var age = 25
+    age = 30
+    print("--- Variables ---")
+    print("name: \(name)")
+    print("age: \(age)")
+}
